89030cd332e7263c65b7fef09b6e73703483a7f0
mourisl <mourisl@hotmail.com>
2019-04-15 19:20:14 -0400

    Add the script that keep trustworthy assemblies based on annotation

---
+++ FilterAnnotatedAssembly.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+
+use strict ;
+use warnings ;
+
+die "usage: FilterAnnotatedAssembly.pl annot.out > filter_annot.out\n" if ( @ARGV == 0 ) ;
+
+open FP1, $ARGV[0] ;
+while ( <FP1> )
+{
+	my $header ;
+	my $seq ;
+	chomp ;
+	$header = $_ ;
+	$seq = <FP1> ;
+	chomp $seq ;
+
+	next if ( $header =~ /\* / || $header =~ /null/ ) ;# Has missing components
+	
+	my @cols = split /\s/, $header ;
+	
+	# Obtain the VJC coordinate
+	my @vCoord ;
+	my @jCoord ;
+	my @cCoord ;
+	my $cdr3 ;
+	if ( $cols[3] =~ /\(([0-9]+?)\):\(([0-9]+?)-([0-9]+?)\):\(([0-9]+?)-([0-9]+?)\)/ )
+	{
+		@vCoord = ($1, $2, $3, $4, $5) ;
+	}
+	else
+	{
+		die "Wrong format $header\n" ;
+	}
+	if ( $cols[4] =~ /\(([0-9]+?)\):\(([0-9]+?)-([0-9]+?)\):\(([0-9]+?)-([0-9]+?)\)/ )
+	{
+		@jCoord = ($1, $2, $3, $4, $5) ;
+	}
+	else
+	{
+		die "Wrong format $header\n" ;
+	}
+	if ( $cols[5] =~ /\(([0-9]+?)\):\(([0-9]+?)-([0-9]+?)\):\(([0-9]+?)-([0-9]+?)\)/ )
+	{
+		@cCoord = ($1, $2, $3, $4, $5 ) ;
+	}
+	else
+	{
+		die "Wrong format $header\n" ;
+	}
+	$cdr3 = ( split /=/, $cols[6] )[1] ;
+
+	#if ( scalar( @vCoord ) < 5 || scalar( @jCoord ) < 5 || scalar( @cCoord ) < 5 ) 
+	#{
+	#	print( "ERROR ", @vCoord, " ", @jCoord, " ", @cCoord, "\n" ) ;
+	#	exit() ;
+	#}
+
+	# The VJC coordinate should be correct
+	next if ( $vCoord[2] > $jCoord[1] || $jCoord[2] > $cCoord[1] + 6 ) ;
+
+	# Each should end within reasonable range in the annotation.
+	next if ( $vCoord[4] < $vCoord[0] - length( $cdr3 ) || $jCoord[3] > length( $cdr3 ) 
+		|| $jCoord[4] < $jCoord[0] - 20 || $cCoord[3] > 20 ) ;
+
+	print "$header\n$seq\n" ;
+}
+close FP1 ;
