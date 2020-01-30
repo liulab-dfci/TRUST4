2eb6a21d9cd3d93303417815b035aceb543bea3c
Li <mourisl@hotmail.com>
2020-01-29 19:22:34 -0500

    Preliminary implementation of lineage tracing taking the isotype into account

--- CloneEvolution.cpp
+++ CloneEvolution.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include <vector>
+#include <algorithm>
 #include <string>
 #include <map>
 
@@ -13,15 +14,29 @@
 char usage[] = "Usage: clone-evo xxx_cdr3.out [OPTIONS] > yyy_evo.out\n"
 	"Options:\n"
 	"\t-a FLOAT: only consider clonotypes more abundant than specified value (default: 2.0)\n"
+	"\t-i STRING: the path to the file containing IGH isotype order, one per line (default: human order)\n"
 	;
 
 struct _cdr3
 {
 	char *seq ;
+	int isotype ; 
 	int clusterId ; // belongs to this cluster
 	int subId ; // the id within this cluster.
 	double abund ;
 	double similarity ;
+
+	bool operator<( const struct _cdr3 &b ) const
+	{
+		if ( clusterId != b.clusterId )
+			return clusterId < b.clusterId ;
+		else if ( isotype != b.isotype )
+			return isotype < b.isotype ;
+		else if ( similarity != b.similarity )
+			return similarity > b.similarity ;
+
+		return false ;
+	}
 } ;
 
 struct _adj
@@ -48,22 +63,49 @@ void InsertAdj( int u, int v, struct _adj *adj, int &adjUsed )
 	++adjUsed ;
 }
 
+bool IsCompatibleLevel( int level, int isotype )
+{
+	if ( isotype == -1 || level == -1 )
+		return true ;
+	return level == isotype ;
+}
+
 // Prim algorithm for minimum spanning tree
-void Prim( int **dist, int n,  struct _adj *adj, int &adjUsed, int offset, std::vector<struct _cdr3> &cdr3s )
+// The algorithm is adjust to put the earlier isotypes aroudn the same levels.
+int Prim( int **dist, int n,  struct _adj *adj, int &adjUsed, int offset, std::vector<struct _cdr3> &cdr3s )
 {
 	int i, j ;
 	struct _pair *minDist = new struct _pair[n] ; // a-dist, b-the node used to connect
 	bool *used = new bool[n] ;
 	memset( used, false, sizeof(bool) * n ) ;
-
-	int max = -1 ;
-	int maxTag = -1 ;
+	
+	int maxIsotype = cdr3s[n - 1].isotype ;
+	int minIsotype = -1 ;
+	int currLevelStart = 0 ;
 	for ( i = 0 ; i < n ; ++i )
-		if ( cdr3s[i + offset].abund > max )
+		if ( cdr3s[i].isotype != -1 )
 		{
-			max = cdr3s[i + offset].abund ;
+			minIsotype = cdr3s[i].isotype ;
+			currLevelStart = i ;
+			break ;
+		}
+
+	double max = -2 ;
+	int maxTag = -1 ;
+	int currLevel = minIsotype ;
+
+	for ( i = 0 ; i < n ; ++i )
+	{
+		if ( !IsCompatibleLevel( currLevel, cdr3s[i + offset].isotype ) )
+			continue ;
+		if ( cdr3s[i + offset].similarity > max 
+			|| (cdr3s[i + offset].similarity == max 
+				&& cdr3s[i + offset].abund > cdr3s[maxTag + offset].abund ) )
+		{
+			max = cdr3s[i + offset].similarity ;
 			maxTag = i ;
 		}
+	}
 
 	minDist[maxTag].a = 0 ;
 	minDist[maxTag].b = 0 ;
@@ -80,9 +122,34 @@ void Prim( int **dist, int n,  struct _adj *adj, int &adjUsed, int offset, std::
 	{
 		int min = LINE_WIDTH ;
 		int minTag = -1 ;
+		
+		// Test whether current level has already been used up.
+		bool goNextLevel = true ;
+
+		for ( j = currLevelStart ; j < n ; ++j )
+		{
+			if ( cdr3s[j + offset].isotype != currLevel )
+				break ;
+			if ( !used[j] && cdr3s[j + offset].isotype == currLevel )
+			{
+				goNextLevel = false ;
+				break ;
+			}
+		}
+		if ( goNextLevel )
+		{
+			if ( j < n )
+				currLevel = cdr3s[j + offset].isotype ;
+			else
+				currLevel = -1 ;
+			currLevelStart = j ;
+		}
+
 		for ( j = 0 ; j < n ; ++j )
 		{
-			if ( !used[j] )
+			if ( cdr3s[j + offset].isotype > currLevel )
+				break ;
+			if ( !used[j] ) // The currLevel condition make sure that earlier isotypes are all used.
 			{
 				if ( minDist[j].a < min )
 				{
@@ -120,6 +187,8 @@ void Prim( int **dist, int n,  struct _adj *adj, int &adjUsed, int offset, std::
 
 	delete[] used ;
 	delete[] minDist ;
+	
+	return maxTag ; 
 }
 
 // Return: whether it is a leaf.
@@ -142,6 +211,7 @@ int LongestRootLeafDistMST( int tag, struct _adj *adj, int n, bool *visited, int
 	return ret ;
 }
 
+//Newick tree format
 void PrintMST( FILE *fp, int tag, int parent, bool *visited, int **dist, int offset, struct _adj *adj, 
 	std::vector<struct _cdr3> &cdr3, std::vector<std::string> clusterIdToName )
 {
@@ -172,6 +242,30 @@ void PrintMST( FILE *fp, int tag, int parent, bool *visited, int **dist, int off
 			cdr3[tag + offset].subId, cdr3[tag + offset].abund, dist[parent][tag] ) ;
 }
 
+// 0-not from ig. 1-from heavy chain. 2-from light chain.
+int GetIgType(char *v, char *j, char *c)
+{
+	char *p ;
+	if ( v[0] != '*' )
+		p = v ;
+	else if ( j[0] != '*' )
+		p = j ;
+	else if ( c[0] != '*' )
+		p = c ;
+	else
+		return 0 ;
+
+	if ( p[0] != 'I' || p[1] != 'G' )
+		return 0 ;
+
+	if ( p[2] == 'H' )
+		return 1 ;
+	else
+		return 2 ;
+	
+	return 0 ;
+}
+
 
 char buffer[LINE_WIDTH] ;
 char buffer2[LINE_WIDTH] ;
@@ -192,6 +286,8 @@ int main( int argc, char *argv[] )
 
 	double minAbund = 2 ;
 
+	std::map<std::string, int> isotypeRank ;
+
 	for ( i = 2 ; i < argc ; ++i )
 	{
 		if ( !strcmp( argv[i], "-a" ) )
@@ -199,7 +295,20 @@ int main( int argc, char *argv[] )
 			minAbund = atof( argv[i + 1] ) ;
 			i += 1 ;
 		}
+		else 
+		{
+			fprintf( stderr, "Unknown parameter %s\n", argv[i] ) ;
+			exit( 1 ) ;
+		}
 	}
+
+	if ( isotypeRank.size() == 0 )
+	{
+		char humanIsotype[9][6] = {"IGHM", "IGHD", "IGHG3", "IGHG1", "IGHA1", "IGHG2", "IGHG4", "IGHE", "IGHA2"} ;
+		for ( i = 0 ; i < 9 ; ++i )
+			isotypeRank[ std::string(humanIsotype[i]) ] = i ;
+	}
+	isotypeRank[ std::string("*") ] = -1 ;
 
 	FILE *fp ;
 	fp = fopen( argv[1], "r" ) ;
@@ -208,15 +317,21 @@ int main( int argc, char *argv[] )
 	while ( fgets( buffer, sizeof(buffer), fp ) != NULL )
 	{
 		char clusterIdBuffer[1024]  ;
+		char vGene[1024], jGene[1024], cGene[1024] ;
 		double score, abund, similarity ;
 		sscanf( buffer, "%s %d %s %s %s %s %s %s %s %lf %lf %lf", clusterIdBuffer, &k, 
-			buffer2, buffer2, buffer2, buffer2,
+			vGene, buffer2, jGene, cGene,
 			buffer2, buffer2, seq,
 			&score, &abund, &similarity ) ;
 
 		if ( score == 0 )
 			continue ;
 
+		// Only IG chains need evolution 
+		int igType = GetIgType( vGene, jGene, cGene ) ;
+		if ( igType == 0 )
+			continue ;
+		
 		struct _cdr3 nc ;
 		nc.seq = strdup( seq ) ;
 		int clusterId ;
@@ -235,6 +350,10 @@ int main( int argc, char *argv[] )
 		nc.subId = k ;
 		nc.abund = abund ;
 		nc.similarity = similarity ;
+		if ( igType == 2 )
+			nc.isotype = 0 ;
+		else
+			nc.isotype = isotypeRank[std::string(cGene)] ;
 		
 		for ( i = 0 ; seq[i] ; ++i )
 			if ( seq[i] == 'N' )
@@ -247,6 +366,8 @@ int main( int argc, char *argv[] )
 		cdr3s.push_back( nc ) ;
 	}
 	
+	std::sort( cdr3s.begin(), cdr3s.end() ) ;
+
 	// Process cluster by cluster. Assume the input is already sorted by cluster
 	int cdr3Cnt = cdr3s.size() ;
 	for ( i = 0 ; i < cdr3Cnt ; )
@@ -278,7 +399,7 @@ int main( int argc, char *argv[] )
 		adjUsed = k ;
 		
 		// Build the tree
-		Prim( dist, n, adj, adjUsed, i, cdr3s ) ;
+		int bestRoot = Prim( dist, n, adj, adjUsed, i, cdr3s ) ;
 
 		// Find the root for the tree
 		std::vector<int> leaves ;
@@ -288,19 +409,19 @@ int main( int argc, char *argv[] )
 				leaves.push_back( k ) ;
 		}
 
-		int bestRoot = -1 ;
-		int maxSimilarity = 0 ;
-		int minMaxRootLeafDist = LINE_WIDTH ;
-		for ( k = 0 ; k < n ; ++k )
-		{
-			if ( cdr3s[i + k].similarity > maxSimilarity )
-				maxSimilarity = cdr3s[i + k].similarity ;
-		}
 		bool *visited = new bool[n] ;
+		memset( visited, false, sizeof( bool ) * n ) ;
+		int minMaxRootLeafDist = LongestRootLeafDistMST( bestRoot, adj, n, visited, dist ) ;
+		// Adjust the best root.
 		for ( k = 0 ; k < n ; ++k )
 		{
-			if ( cdr3s[i + k].similarity < maxSimilarity )
+			if ( cdr3s[i + k].similarity < cdr3s[i + bestRoot].similarity )
 				continue ;
+			if ( !IsCompatibleLevel( cdr3s[bestRoot + k].isotype, cdr3s[i + k].isotype ) )
+				continue ;
+			if ( k == bestRoot )
+				continue ;
+
 			memset( visited, false, sizeof( bool ) * n ) ;
 			int max = LongestRootLeafDistMST( k, adj, n, visited, dist ) ;
 			if ( max < minMaxRootLeafDist )
