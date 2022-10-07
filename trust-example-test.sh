d23f5caed5b9f3286287710d19133a11a9c272f9
Li <mourisl@hotmail.com>
2022-10-07 18:18:09 -0400

    Add the script to test whether TRUST4 can be properly executed. Update the results from the example file.

---
+++ trust-example-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+./run-trust4 -f hg38_bcrtcr.fa --ref human_IMGT+C.fa -b example/example.bam -o example_test
+
+if [ ! -e "example_test_report.tsv" ]; then
+	echo "Failed to run TRUST4. Please check whether TRUST4 is compiled with command 'make' or the other files are in the right paths."	
+	exit
+fi
+
+if [[ $(diff example_test_report.tsv example/TRUST_example_report.tsv | wc -l) -ge 1 ]]; then
+	echo "Results do not match the pregenerated example results. Please check TRUST4's version or the example files are in the folder."	
+	exit
+fi
+
+rm example_test_*
+
+echo "TRUST4 is ready to use."
