b88760392062983d4098be04aa5894f250ba2fcc
Rudolph-afk <abel874@live.com>
2023-05-05 12:15:28 +0200

    +https for IMGT website

--- BuildImgtAnnot.pl
+++ BuildImgtAnnot.pl
@@ -11,13 +11,13 @@ sub system_call
 	system(@_) == 0
 		or die "system @_ failed: $?";
 	#print STDERR " finished\n";
-} 
+}
 
 my $species = $ARGV[0] ;
 # Download
 #system_call( "wget -np -nd -r -A fasta -P tmp_download http://www.imgt.org//download/V-QUEST/IMGT_V-QUEST_reference_directory/".$species."/" ) ;
 #system_call( "cat tmp_download/*.fasta > tmp_download/vquest.fa") ;
-system_call( "wget -O IMGT_download.fa http://www.imgt.org/download/GENE-DB/IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP" ) ;
+system_call( "wget -O IMGT_download.fa https://www.imgt.org/download/GENE-DB/IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP" ) ;
 
 # Reformat
 #>J00256|IGHJ1*01|Homo sapiens|F|J-REGION|723..774|52 nt|1| | | | |52+0=52| | |
@@ -25,8 +25,8 @@ system_call( "wget -O IMGT_download.fa http://www.imgt.org/download/GENE-DB/IMGT
 open FP1, "IMGT_download.fa" ;
 my $prevId = "" ;
 my $prevGeneName = "" ;
-my $output = 0 ; 
-my $skipHeader = 0 ; # For the genes that are split into pieces in the database. 
+my $output = 0 ;
+my $skipHeader = 0 ; # For the genes that are split into pieces in the database.
 
 while ( <FP1> )
 {
